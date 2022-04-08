764a9d32f7c47505f8f1402bf88e734a3a3ff493
Branislav Ristic <risticbr021@gmail.com>
2022-04-08 20:06:32 +0200

    feat: Repository,Service,Controller (no impl)

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Controller/RoomController.cs
@@ -0,0 +1,51 @@
+﻿using Projekat_SIMS_IN_TIM3.Model;
+using Projekat_SIMS_IN_TIM3.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_SIMS_IN_TIM3.Controller
+{
+    public class RoomController
+    {
+        public Room GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Room> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Split(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Merge(int firstId, int secondId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoomService roomService;
+
+    }
+}
---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Service/RoomService.cs
@@ -0,0 +1,51 @@
+﻿using Projekat_SIMS_IN_TIM3.Model;
+using Projekat_SIMS_IN_TIM3.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_SIMS_IN_TIM3.Service
+{
+    public class RoomService
+    {
+        public Room GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Room> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Split(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Merge(int firstId, int secondId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoomRepository roomRepository;
+
+    }
+}
