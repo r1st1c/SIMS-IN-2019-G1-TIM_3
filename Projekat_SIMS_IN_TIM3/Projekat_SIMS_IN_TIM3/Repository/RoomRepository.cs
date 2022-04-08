f88196dadd2594a60da0e071c1e405e69958a481
Branislav Ristic <risticbr021@gmail.com>
2022-04-08 20:30:48 +0200

    fix: include in RoomRepository

--- Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Repository/RoomRepository.cs
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Repository/RoomRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projekat_SIMS_IN_TIM3.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,41 +8,41 @@ using System.Threading.Tasks;
 namespace Projekat_SIMS_IN_TIM3.Repository
 {
     public class RoomRepository
-    {
-        public Model.Room GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Model.Room> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Create(Model.Room room)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Update(Model.Room room)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool DeleteById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Split(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Merge(int firstId, int secondId)
-        {
-            throw new NotImplementedException();
-        }
-
-    }
+   {
+      public Room GetById(int id)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public List<Room> GetAll()
+      {
+         throw new NotImplementedException();
+      }
+      
+      public bool Create(Room room)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public bool Update(Room room)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public bool DeleteById(int id)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public bool Split(int id)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public bool Merge(int firstId, int secondId)
+      {
+         throw new NotImplementedException();
+      }
+   
+   }
 }
