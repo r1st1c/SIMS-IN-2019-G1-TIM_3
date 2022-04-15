9759577b79f1f2abcbe3ad437ef7c89f3a5924bb
Branislav Ristic <risticbr021@gmail.com>
2022-04-15 09:39:15 +0200

    typo error

--- Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/PatientXAML/UpdateAndDeleteAppointment.xaml.cs
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/PatientXAML/UpdateAndDeleteAppointment.xaml.cs
@@ -84,7 +84,7 @@ namespace Projekat_SIMS_IN_TIM3.PatientXAML
 
             
             application.appointmentController.CreateAppointment(newAppointment);
-            MessageBox.Show("SuSuccessfully updated appointment ");
+            MessageBox.Show("Successfully updated appointment ");
 
 
             /* 
