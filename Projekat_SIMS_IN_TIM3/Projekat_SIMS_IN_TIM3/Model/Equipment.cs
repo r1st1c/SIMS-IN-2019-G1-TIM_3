637f2210cd7a865027737aa592afa558c4672ecf
Branislav Ristic <risticbr021@gmail.com>
2022-04-16 12:08:58 +0200

    added equipment class

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Model/Equipment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_SIMS_IN_TIM3.Model
+{
+    internal class Equipment
+    {
+        public enum EquipmentType
+        {
+            static_equipment,
+            dynamic_equipment
+        }
+
+        public string Equipmentname { get; set; }
+        public string Manufacturer { get; set; }
+        public EquipmentType Equipmenttype { get; set; }
+        public int Id { get; set; }
+
+
+
+
+    }
+}
