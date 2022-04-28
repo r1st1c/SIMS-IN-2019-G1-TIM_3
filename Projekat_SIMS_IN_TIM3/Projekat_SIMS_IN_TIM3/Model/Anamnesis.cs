5e3d47c67884d91014e71ac60ac9b318569266fe
Luka <jovanovic.luka1717@gmail.com>
2022-04-28 22:08:10 +0200

    Added anamnesis class to model

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Model/Anamnesis.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_SIMS_IN_TIM3.Model
+{
+
+
+    public class Anamnesis
+    {
+        public Anamnesis()
+        {
+            // TODO: implement
+        }
+
+        public Anamnesis(int Id, int PatientId, int DoctorId, DateTime DateAndTime, AppointmentType AppType, String PercievedDifficulties, String GeneralConclusion)
+        {
+            this.Id = Id;
+            this.PatientId = PatientId; 
+            this.DoctorId = DoctorId;   
+            this.DateAndTime = DateAndTime; 
+            this.AppType = AppType; 
+            this.PerceivedDifficulties = PercievedDifficulties;
+            this.GeneralConclusion = GeneralConclusion;
+        }
+
+        ~Anamnesis()
+        {
+            // TODO: implement
+        }
+
+        private int Id { get; set; }
+        private int PatientId { get; set;}
+        private int DoctorId { get; set;}
+
+        private DateTime DateAndTime { get; set; }
+
+        private AppointmentType AppType { get; set; }
+        private String PerceivedDifficulties { get; set; }
+        private String GeneralConclusion { get; set; }
+
+    }
+
+
+
+}
