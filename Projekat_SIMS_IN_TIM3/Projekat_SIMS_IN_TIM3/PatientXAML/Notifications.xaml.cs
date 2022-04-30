402347e12bfcde91b12264e159b39e914e4a42cb
Luka <jovanovic.luka1717@gmail.com>
2022-04-30 16:51:30 +0200

    Added search appointments by priority on  backend

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/PatientXAML/Notifications.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Projekat_SIMS_IN_TIM3.Model;
+using Projekat_SIMS_IN_TIM3.Controller;
+
+namespace Projekat_SIMS_IN_TIM3.PatientXAML
+{
+    /// <summary>
+    /// Interaction logic for Notifications.xaml
+    /// </summary>
+    public partial class Notifications : Window
+    {
+        public App application { get; set; }
+        public MedicalRecord medRecord { get; set; }
+        // da li medical record ili list<medicine prescription>
+        public Notifications()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+            application = Application.Current as App;
+        }
+    }
+}
