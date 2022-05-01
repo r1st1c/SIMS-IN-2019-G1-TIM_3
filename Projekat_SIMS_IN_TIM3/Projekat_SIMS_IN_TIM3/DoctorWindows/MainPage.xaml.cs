0fbd90cc0a1da85a83d4f9dd262c9b578f310089
unknown <elenaakik11@gmail.com>
2022-05-01 04:25:13 +0200

    KT3: fixed mainPage, added links to other pages

--- Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/DoctorWindows/MainPage.xaml.cs
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/DoctorWindows/MainPage.xaml.cs
@@ -23,5 +23,40 @@ namespace Projekat_SIMS_IN_TIM3.DoctorWindows
         {
             InitializeComponent();
         }
+
+        private void HomeBtn(object sender, RoutedEventArgs e)
+        {
+            MainPage mainPage = new MainPage();
+            mainPage.Show();
+        }
+        private void CalendarPageBtn(object sender, RoutedEventArgs e)
+        {
+            Calendar calendar = new Calendar();
+            calendar.Show();
+        }
+
+        private void NotifBtn(object sender, RoutedEventArgs e)
+        {
+            Notifications notifications = new Notifications();
+            notifications.Show();
+        }
+
+        private void ListOfMedBtn(object sender, RoutedEventArgs e)
+        {
+            ListOfMedicines listOfMedicines = new ListOfMedicines();
+            listOfMedicines.Show();
+        }
+
+        private void MedVerifBtn(object sender, RoutedEventArgs e)
+        {
+            MedVerif medVerif = new MedVerif();
+            medVerif.Show();
+        }
+
+        private void AbsenceReqBtn(object sender, RoutedEventArgs e)
+        {
+            CreateAbsenceReq createAbsenceReq = new CreateAbsenceReq();
+            createAbsenceReq.Show();
+        }
     }
 }
