738142384547a9ad0a4550572d74e83315f41752
unknown <mirkovic0705@gmail.com>
2022-05-06 00:08:13 +0200

    secretary allergens

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Controller/AllergenController.cs
@@ -0,0 +1,46 @@
+﻿using Projekat_SIMS_IN_TIM3.Model;
+using Projekat_SIMS_IN_TIM3.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_SIMS_IN_TIM3.Controller
+{
+    public class AllergenController
+    {
+        private AllergenService allergenService = new AllergenService();
+
+
+        public List<Allergen> GetAll()
+        {
+            return allergenService.GetAll();
+        }
+
+        public List<Allergen> GetById(int id)
+        {
+            return allergenService.GetById(id);
+        }
+
+        public void Save(Allergen allergen)
+        {
+            allergenService.Save(allergen);
+        }
+
+        public void Delete(int allergen)
+        {
+            allergenService.Delete(allergen);
+        }
+        
+        public void Update(Allergen allergen)
+        {
+            allergenService.Update(allergen);
+        }
+
+        public List<Allergen> GetByPatientsId(int patientId)
+        {
+            return this.allergenService.GetByPatientsId(patientId);
+        }
+    }
+}
---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/SecretaryXAML/SecretaryAllergens.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Projekat_SIMS_IN_TIM3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekat_SIMS_IN_TIM3.SecretaryXAML
+{
+    /// <summary>
+    /// Interaction logic for SecretaryAllergens.xaml
+    /// </summary>
+    public partial class SecretaryAllergens : Window
+    {
+        App application;
+        List<Allergen> allergens = new List<Allergen>();
+        Allergen selectedAllergen;
+        int maxId = 0;
+        public SecretaryAllergens()
+        {
+            InitializeComponent();
+            application = Application.Current as App;
+            allergens = application.allergenController.GetAll();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            maxId++;
+            var newAllergen = new Allergen(maxId,Convert.ToInt32(txtPatientId.Text),txtName.Text,txtDetails.Text);
+            application.allergenController.Save(newAllergen);
+
+            txtAllergenId.Text = "";
+            txtName.Text = "";
+            txtDetails.Text = "";
+            txtPatientId.Text = "";
+            MessageBox.Show("Successfully");
+
+
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            int allergenId = Convert.ToInt32(txtAllergenId.Text);
+            application.allergenController.Delete(allergenId);
+            var newAllergen = new Allergen(maxId+1, Convert.ToInt32(txtPatientId.Text), txtName.Text, txtDetails.Text);
+            application.allergenController.Save(newAllergen);
+
+            txtAllergenId.Text = "";
+            txtName.Text = "";
+            txtDetails.Text = "";
+            txtPatientId.Text = "";
+            MessageBox.Show("Successfully");
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            int patientId = Convert.ToInt32(txtPatientId.Text);
+            allergens = application.allergenController.GetByPatientsId(patientId);
+            List<Allergen> allergenList = new List<Allergen>();
+            foreach(Allergen allergen in allergens)
+            {
+                if(allergen.patietId == patientId)
+                {
+                    allergenList.Add(allergen);
+                }
+            }
+            
+            DataBinding1.ItemsSource = allergenList;
+        }
+    }
+}
