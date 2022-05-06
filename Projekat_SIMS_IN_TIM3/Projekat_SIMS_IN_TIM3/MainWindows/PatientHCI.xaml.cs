4b8dedae23a5e2b214d3475cf6ea7ecc891b1b48
Luka <jovanovic.luka1717@gmail.com>
2022-05-06 10:54:38 +0200

    Added window for hci

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/MainWindows/PatientHCI.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekat_SIMS_IN_TIM3.MainWindows
+{
+    /// <summary>
+    /// Interaction logic for PatientHCI.xaml
+    /// </summary>
+    public partial class PatientHCI : Window
+    {
+        public PatientHCI()
+        {
+            InitializeComponent();
+        }
+    }
+}
