35e3412873b9e2a4f9c85e94949a97fdc35c040c
Luka <jovanovic.luka1717@gmail.com>
2022-05-06 13:14:25 +0200

    Added notification class to model

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Model/Notification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_SIMS_IN_TIM3.Model
+{
+    public class Notification
+    {
+        public Notification()
+        {
+            // TODO: implement
+        }
+
+        public Notification(int Id, String MedicineName, DateTime Time)
+        {
+            this.Id = Id;
+            this.MedicineName = MedicineName;
+            this.Time = Time;
+        }
+
+
+
+        ~Notification()
+        {
+            // TODO: implement
+        }
+
+        public int Id { get; set; }
+        public String MedicineName { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
