3437af76beb3e5d507a6344f7c0055b8fd47264f
Branislav Ristic <risticbr021@gmail.com>
2022-05-17 14:12:02 +0200

    added medicineingredient and its crud

---
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/Model/MedicineIngredient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_SIMS_IN_TIM3.Model
+{
+    public class MedicineIngredient
+    {
+        public MedicineIngredient(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
