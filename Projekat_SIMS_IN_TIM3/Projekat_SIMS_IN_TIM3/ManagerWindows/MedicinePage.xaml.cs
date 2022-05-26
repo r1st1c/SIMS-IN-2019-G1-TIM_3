8b37ad6df04a428936beedde7b8f436dde123f49
Branislav Ristic <risticbr021@gmail.com>
2022-05-26 20:52:44 +0200

    separated unverified and rejected medicine

--- Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/ManagerWindows/MedicinePage.xaml.cs
+++ Projekat_SIMS_IN_TIM3/Projekat_SIMS_IN_TIM3/ManagerWindows/MedicinePage.xaml.cs
@@ -1,9 +1,5 @@
-﻿using Projekat_SIMS_IN_TIM3.Controller;
-using Projekat_SIMS_IN_TIM3.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,24 +20,21 @@ namespace Projekat_SIMS_IN_TIM3.ManagerWindows
     /// </summary>
     public partial class MedicinePage : Page
     {
-        public ObservableCollection<Medicine> all { get; set; } = new ObservableCollection<Medicine>();
-        public MedicineController medicineController = new MedicineController();
         public MedicinePage()
         {
-            /*List<MedicineIngredient> brufenIngredients = new List<MedicineIngredient>();
-            brufenIngredients.Add(new MedicineIngredient("Ibuprofen"));
-            Medicine toAdd = new Medicine(0, "Brufen", brufenIngredients,true,null);
-            this.medicineController.Create(toAdd);*/
             InitializeComponent();
+            MedicineFrame.Content = new UnapprovedMedicinePage();
             this.DataContext = this;
-            var list = this.medicineController.GetAll();
-            all = new ObservableCollection<Medicine>(list);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Unapproved_Click(object sender, RoutedEventArgs e)
         {
-            var add = new AddMedicineWindow(all);
-            add.ShowDialog();
+            MedicineFrame.Content = new UnapprovedMedicinePage();
+        }
+
+        private void Rejected_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
